886e625722e5284517a3c6826e0ad9db81eb315e
Carlos Flores <carlos_raul_98@outlook.com>
2021-03-20 10:36:25 -0700

    Refactor code

--- app/bin/app.py
+++ app/bin/app.py
@@ -1,29 +1,102 @@
 from argparse import ArgumentParser
 import numpy as np
-from app.lib import limitJson, openStack, test, mdpo, load, shot
+from app.lib import Measures, load
+from prettytable import PrettyTable
 
-def compare(v1, v2, offset=0.150, debug=False):
-    if not v1 or not v2:
-        return None
-    cv1, cv2 = v1.copy(), v2.copy()
-    start, end, agree = None, None, 0
-    va1 = [0 for v in cv1]
+class Dex(object):
+    def __init__(self, buffer, observer, offset=0.150, debug=False):
+        self.table = PrettyTable()
+        self.files = buffer
+        self.offset = offset
+        self.debug = debug
+        self.ratingtask = None
+        self.assest = None
+        self.comp1, self.comp2 = None, None
+        
+        if observer == 0:
+            self.__observer1()
+        elif observer == 1:
+            self.__observer2()
+        else:
+            raise NotImplementedError('Only supported 2 observers')
     
-    xPlot, yPlot = [], [-1 for v in cv1]
-    for i, e1 in enumerate(cv1):
-        xPlot.append(round(e1[0]))
-        for e2 in cv2:
-            diffStart, diffEnd = abs(e1[0] - e2[0]), abs(e1[1] - e2[1])
-            if i == 2 and debug:
-                print(e1, e2)
-                print(diffStart, diffEnd, diffStart >= 0, diffEnd >= 0, diffEnd <= offset, e1[2] == e2[2])
-            if diffStart >= 0 and diffStart <= offset and diffEnd >= 0 and diffEnd <= offset and e1[2] == e2[2]:
-                if debug: print(diffStart, diffEnd, e1[2])
-                yPlot[i] = round(e2[0])
-                agree += 1
-                va1[i] += 1
-                cv2.remove(e2)
-    return agree/len(cv1)*100, [agree, len(cv1)], va1, xPlot, yPlot
+    def __observer1(self):
+        v1, v2 = ((k, v) for k, v in self.files.items())
+        ioa, attemps, self.assest, self.comp1, self.comp2 = self.__compare(v1[1], v2[1])
+        for v in [[v1[0]] + self.comp1]:
+            self.table.add_row(v)
+        for v in [[v2[0]] + self.comp2]:
+            self.table.add_row(v)
+        print('\n{}, attemps: {}'.format(v1[0], attemps))
+        self.__test(self.comp1, self.comp2)
+    
+    def __observer2(self):
+        v1, v2 = ((k, v) for k, v in self.files.items())
+        ioa, attemps, assest, self.comp1, self.comp2 = self.__compare(v2[1], v1[1])
+        self.table.add_row([[v1[0]] + self.comp2])
+        self.table.add_row([[v2[0]] + self.comp1])
+        print('\n{}, attemps: {}'.format(v2[0], attemps))
+        self.__test(self.comp1, self.comp2)
+    
+    def __compare(self, v1, v2):
+        if not v1 or not v2:
+            return None
+        cv1, cv2 = v1.copy(), v2.copy()
+        start, end, agree = None, None, 0
+        va1 = [0 for v in cv1]
+        
+        xPlot, yPlot = [], [-1 for v in cv1]
+        for i, e1 in enumerate(cv1):
+            xPlot.append(round(e1[0]))
+            for e2 in cv2:
+                diffStart, diffEnd = abs(e1[0] - e2[0]), abs(e1[1] - e2[1])
+                if i == 2 and self.debug:
+                    print(e1, e2)
+                    print(diffStart, diffEnd, diffStart >= 0, diffEnd >= 0, diffEnd <= self.offset, e1[2] == e2[2])
+                if diffStart >= 0 and diffStart <= self.offset and diffEnd >= 0 and diffEnd <= self.offset and e1[2] == e2[2]:
+                    if self.debug: print(diffStart, diffEnd, e1[2])
+                    yPlot[i] = round(e2[0])
+                    agree += 1
+                    va1[i] += 1
+                    cv2.remove(e2)
+        return agree/len(cv1)*100, [agree, len(cv1)], va1, xPlot, yPlot
+    
+    def __test(self, v1, v2):
+        from sklearn.metrics import cohen_kappa_score
+        from nltk import agreement
+        #import pingouin as pg
+        #import pandas as pd
+        formatted = [[1, i, v] for i, v in enumerate(v1)] + [[2, i, v] for i, v in enumerate(v2)]
+        self.ratingtask = agreement.AnnotationTask(data=formatted)
+        #data = pd.DataFrame(data={'x':x, 'y':y})
+        #cronbach = pg.cronbach_alpha(data=data)
+    
+    def showAssest(self):
+        print('Assest: {}'.format(self.assest))
+    
+    def showComparation(self):
+        print('Observer 1: {}\nObserver 2: {}\n'.format(self.comp1, self.comp2))
+    
+    def showTable(self):
+        print(self.table)
+    
+    def kappa(self, printRes=True):
+        kappa = self.ratingtask.kappa()
+        if printRes:
+            print('Cohen\'s Kappa: {}%'.format(round(kappa * 100, 4)))
+        return kappa
+    
+    def kalpha(self, printRes=True):
+        alpha = self.ratingtask.alpha()
+        if printRes:
+            print('Krippendorff\'s alpha: {}%'.format(round(alpha * 100, 4)))
+        return alpha
+    
+    def spi(self, printRes=True):
+        pi = self.ratingtask.pi()
+        if printRes:
+            print('Scott\'s pi: {}%'.format(round(pi * 100, 4)))
+        return pi
 
 def getArgs():
     parser = ArgumentParser(prog='deadshot', description='Arguments being passed to the program')
@@ -31,61 +104,29 @@ def getArgs():
     parser.add_argument('--sample', '-s', required=True, default='./sample', help='Sample path')
     return parser.parse_args()
 
-def graph(x, y):
-    # https://plotly.com/python/parallel-coordinates-plot/
-    import plotly.express as px
-    df = px.data.iris()
-    fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
-                "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
-                "petal_width": "Petal Width", "petal_length": "Petal Length", },
-                color_continuous_scale=px.colors.diverging.Tealrose,
-                color_continuous_midpoint=2)
-    fig.show()
-
-def test(x, y):
-    from sklearn.metrics import cohen_kappa_score
-    from nltk import agreement
-    import pingouin as pg
-    import pandas as pd
-    formatted = [[1,i,x[i]] for i in range(len(x))] + [[2,i,y[i]] for i in range(len(y))]
-    ratingtask = agreement.AnnotationTask(data=formatted)
-    data = pd.DataFrame(data={'x':x, 'y':y})
-    kappa = ratingtask.kappa() # cohen_kappa_score(x, y)
-    cronbach = pg.cronbach_alpha(data=data)
-    alpha = ratingtask.alpha()
-    pi = ratingtask.pi()
-    return kappa, cronbach, alpha, pi
-
-def showMetrics(v1, v2, ioa):
-    kappa, cronbach, alpha, pi = test(v1, v2)
-    print('*** METRICS ***')
-    print('Exact IOA: {}%'.format(round(ioa, 4)))
-    print('Cronbach\'s alpha: {}% {} - {}'.format(round(cronbach[0] * 100, 4), cronbach[1][0], cronbach[1][1]))
-    print('Cohen\'s Kappa: {}%'.format(round(kappa * 100, 4)))
-    print('Krippendorff\'s alpha: {}%'.format(round(alpha * 100, 4)))
-    print('Scott\'s pi: {}%'.format(round(pi * 100, 4)))
-
 def main():
     args = getArgs()
     print(args)
     
     buffer, bLen = load(path=args.sample)
-    mdpo(buffer)
-    if bLen == 3:
-        v1, v2 = ((k, v) for k, v in buffer.items())
-        ioa, attemps, assest, x, y = compare(v1[1], v2[1])
-        kappa, cronbach, alpha, pi = test(x, y)
-        print('{}, attemps: {}'.format(v1[0], attemps))
-        showMetrics(x, y, ioa)
-        print('Assest: {}'.format(assest))
-        print('Observer 1: {}\nObserver 2: {}\n'.format(x, y))
-        
-        ioa, attemps, assest, x, y = compare(v2[1], v1[1])
-        test(x, y)
-        print('{}, attemps: {}'.format(v2[0], attemps))
-        showMetrics(x, y, ioa)
-        print('Assest: {}'.format(assest))
-        print('Observer 1: {}\nObserver 2: {}\n'.format(x, y))
+    if bLen == 2:
+        dex = Dex(buffer, observer=0)
+        dex.showAssest()
+        dex.kappa()
+        dex.kalpha()
+        dex.spi()
+        dex = Dex(buffer, observer=1)
+        dex.kappa()
+        dex.kalpha()
+        dex.spi()
+    else:
+        raise NotImplementedError('Only accepted two observers')
+    '''z = Measures(buffer, metric='label')
+    z.showTable()
+    z.mdpo()
+    z.kappa()
+    z.alpha()
+    z.pi()'''
 
 if __name__ == '__main__':
     main()
--- app/lib/measures.py
+++ app/lib/measures.py
@@ -1,36 +1,48 @@
 import numpy as np
 from prettytable import PrettyTable
+from nltk import agreement
+
+def hasNumber(s):
+    return any(char.isdigit() for char in s)
 
 class Measures(object):
-    def __init__(self, files, mode='mdpo', header=None):
+    def __init__(self, buffer, offset=0.150, metric='label', header=None):
         self.table = PrettyTable()
-        self.files = files
-        self.mode = mode
+        self.files = buffer
+        self.offset = offset
+        self.ratingtask = None
         self.header = header if header else ['Observer']
-        self.body = []
+        self.tableBody = []
         self.countLabels = []
         self.nIntervals = 0
         
-        self._checkIsSameHeader()
-        self._setHeader()
-        self._process()
-    def _checkIsSameHeader(self):
-        cluster = [sorted(v, key=lambda v : v[2]) for k, v in self.files.items()]
-        columnLabelCluster = [np.array(labelTrack)[:,2] for labelTrack in cluster]
-        preHeaderCluster = [np.unique(v, axis=0) for v in columnLabelCluster]
-        headerCluster = [[v[0] if any(char.isdigit() for char in v) else v for v in item] for item in preHeaderCluster]
-        headerClusterLen = np.array([len(item) for item in headerCluster])
-        isSameHeader = np.all(headerClusterLen == headerClusterLen[0])
-        if not isSameHeader:
-            raise NotImplementedError('Header doesnt match')
-    def _setHeader(self):
-        preHeader = [v[0] if any(char.isdigit() for char in v) else v for v in np.unique(sorted([e[2] for events in self.files.values() for e in events]), axis=0)]
-        self.nIntervals = len(preHeader)
-        self.header += preHeader
-    def _process(self):
+        if metric == 'label':
+            self.__checkLabels()
+            self.__metricForLabel()
+        elif metric == 'start':
+            print('START')
+            self.__metricForStart()
+        else:
+            raise NotImplementedError('Metric no supported')
+    
+    def __checkLabels(self):
+        labels = [np.unique(sorted([e[2] for e in v]), axis=0) for v in self.files.values()]
+        labelsSanitized = [[v[0] if hasNumber(v) else v for v in h] for h in labels]
+        labelsLen = np.array([len(item) for item in labelsSanitized])
+        sameLabels = np.all(labelsLen == labelsLen[0])
+        self.nIntervals = labelsLen[0]
+        if not sameLabels:
+            raise NotImplementedError('Labels doesnt match')
+    
+    def __setTableHeader(self):
+        labels = np.unique(sorted([e[2] for events in self.files.values() for e in events]), axis=0)
+        labelsSanitized = [v[0] if hasNumber(v) else v for v in labels]
+        self.header += labelsSanitized
+    
+    def __metricForLabel(self):
         intervals, labels = [], []
-        currLabel = None
-        count = 0
+        currLabel, count = None, 0
+        
         for k, v in self.files.items():
             owner, labelTrack = k, sorted(v, key=lambda v : v[2])
             for event in labelTrack:
@@ -47,20 +59,61 @@ class Measures(object):
                 intervals.append(count)
                 currLabel = None
                 count = 0
-            self.body += [[owner] + intervals]
+            self.tableBody += [[owner] + intervals]
             self.countLabels.append(intervals)
             intervals = []
+        formatted = [[i + 1, j, cc] for i, c in enumerate(self.countLabels) for j, cc in enumerate(c)]
+        self.ratingtask = agreement.AnnotationTask(data=formatted)
+    
+    def __compareTwoVecs(self, v1, v2, debug=False):
+        v1, v2 = v1[1], v2[1]
+        if not v1 or not v2:
+            return None
+        cv1, cv2 = v1.copy(), v2.copy()
+        start, end, agree = None, None, 0
+        va1 = [0 for v in cv1]
+        
+        xPlot, yPlot = [], [-1 for v in cv1]
+        for i, e1 in enumerate(cv1):
+            xPlot.append(round(e1[0]))
+            for e2 in cv2:
+                diffStart, diffEnd = abs(e1[0] - e2[0]), abs(e1[1] - e2[1])
+                if i == 2 and debug:
+                    print(e1, e2)
+                    print(diffStart, diffEnd, diffStart >= 0, diffEnd >= 0, diffEnd <= self.offset, e1[2] == e2[2])
+                if diffStart >= 0 and diffStart <= self.offset and diffEnd >= 0 and diffEnd <= self.offset and e1[2] == e2[2]:
+                    if debug: print(diffStart, diffEnd, e1[2])
+                    yPlot[i] = round(e2[0])
+                    agree += 1
+                    va1[i] += 1
+                    cv2.remove(e2)
+        return agree/len(cv1)*100, [agree, len(cv1)], va1, xPlot, yPlot
+    
+    def __metricForStart(self):
+        v1, v2 = ((k, v) for k, v in self.files.items())
+        self.__compareTwoVecs(v1, v2)
+        formatted = [[i + 1, j, cc] for i, c in enumerate(self.countLabels) for j, cc in enumerate(c)]
+        self.ratingtask = agreement.AnnotationTask(data=formatted)
+    
     def showTable(self):
+        if metric != 'label':
+            raise NotImplementedError('This method has been implemented for your current metric')
+        self.__setTableHeader()
         self.table.field_names = self.header
-        for row in self.body:
+        for row in self.tableBody:
             self.table.add_row(row)
         print(self.table)
+    
     def totDur(self, printRes=True):
+        if metric != 'label':
+            raise NotImplementedError('This method has been implemented for your current metric')
         sums = np.sum(self.countLabels, axis=1)
         shorterDur, longerDur = np.min(sums), np.max(sums)
-        totalDur = shorterDur / longerDur * 100
+        totalDur = shorterDur / longerDur
         if printRes:
-            print('Total duration is:', totalDur)
+            print('Total duration is: {}%'.format(round(totalDur, 4) * 100))
+        return totalDur
+    
     def mdpo(self, printRes=True):
         '''Mean duration per occurrence (mdpo)
         
@@ -83,58 +136,41 @@ class Measures(object):
         ----------
         https://www.youtube.com/watch?v=JUFh-dNGYwo
         '''
+        if metric != 'label':
+            raise NotImplementedError('This method has been implemented for your current metric')
         shortersDur = np.min(self.countLabels, axis=0)
         longersDur = np.max(self.countLabels, axis=0)
         sumDurIoas = np.sum(shortersDur / longersDur)
-        mdpo = sumDurIoas / self.nIntervals * 100
+        mdpo = sumDurIoas / self.nIntervals
         if printRes:
-            print('Mean duration per occurrence is:', mdpo)
+            print('Mean duration per occurrence is: {}%'.format(round(mdpo, 4) * 100))
+        return mdpo
+    
+    def kappa(self, printRes=True):
+        kappa = self.ratingtask.kappa()
+        if printRes:
+            print('Cohen\'s Kappa: {}%'.format(round(kappa, 4) * 100))
+        return kappa
+    
+    def alpha(self, printRes=True):
+        alpha = self.ratingtask.alpha()
+        if printRes:
+            print('Krippendorff\'s alpha: {}%'.format(round(alpha, 4) * 100))
+        return alpha
+    
+    def pi(self, printRes=True):
+        pi = self.ratingtask.pi()
+        if printRes:
+            print('Scott\'s pi: {}%'.format(round(pi, 4) * 100))
+        return pi
 
-def mdpo(buffer):
-    x = PrettyTable()
-    z = Measures(buffer)
-    z.showTable()
-    z.mdpo()
-    
-    
-    header = ['Observer']
-    header += [v[0] if any(char.isdigit() for char in v) else v for v in np.unique(sorted([e[2] for events in buffer.values() for e in events]), axis=0)]
-    intervals, labels = [], []
-    currLabel = None
-    count = 0
-    body = []
-    for k, v in buffer.items():
-        owner, labelTrack = k, sorted(v, key=lambda v : v[2])
-        for event in labelTrack:
-            label = event[2]
-            if not currLabel:
-                currLabel = label
-            if currLabel != label:
-                intervals.append(count)
-                currLabel = None
-                count = 1
-                continue
-            count += 1
-        else:
-            intervals.append(count)
-            currLabel = None
-            count = 0
-        body += [[owner] + intervals]
-        labels.append(intervals)
-        intervals = []
-    
-    x.field_names = header
-    for row in body:
-        x.add_row(row)
-    print(x)
-    # Total duration
-    sums = np.sum(labels, axis=1)
-    shorterDur, longerDur = np.min(sums), np.max(sums)
-    totalDur = shorterDur / longerDur * 100
-    print('Total duration is:', totalDur)
-    
-    # Mean duration per occurrence
-    shortersDur, longersDur = np.min(labels, axis=0), np.max(labels, axis=0)
-    sumDurIoas = np.sum(shortersDur / longersDur)
-    mdpo = sumDurIoas / len(labels[0]) * 100
-    print('Mean duration per occurrence is:', mdpo)
+def graph(x, y):
+    # https://plotly.com/python/parallel-coordinates-plot/
+    import plotly.express as px
+    df = px.data.iris()
+    fig = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
+                "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
+                "petal_width": "Petal Width", "petal_length": "Petal Length", },
+                color_continuous_scale=px.colors.diverging.Tealrose,
+                color_continuous_midpoint=2)
+    fig.show()

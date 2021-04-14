7548508759dac354d4907beb0b456fb062634530
Carlos Flores <carlos_raul_98@outlook.com>
2021-04-14 15:56:21 -0700

    Update script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,4 @@ env=develop
 sudo python3 setup.py build
 sudo python3 setup.py $env
 set -e
-exec deadshot $@ -aL 90 -s ./docs/labels/test/994
+exec deadshot $@ -aL 90 -sp ./docs/labels/000 -do 0.300 -r RevB

781d1a465e45eab22743bdb1e6c63e9bb40b4fb0
Carlos Flores <carlos_raul_98@outlook.com>
2021-04-19 23:03:43 -0700

    Refactor code

--- app/bin/app.py
+++ app/bin/app.py
@@ -19,21 +19,21 @@ class Dex(object):
         self.sv2 = None
         self.dv1 = None
         self.dv2 = None
+        self.data = {'observer1': [], 'observer2': []}
         self.showAttemps = showAttemps
         self.attemps = 0
         self.oName = None
         self.src = src
-        
-        if observer == 0:
-            self.__observer1()
-        elif observer == 1:
-            self.__observer2()
-        else:
-            raise NotImplementedError('Only supported 2 observers')
+        self.process()
+    
+    def process(self):
+        v1, v2 = ((k, v) for k, v in self.files.items())
+        self.__compare(v1[1], v2[1], 'observer1')
+        self.__compare(v2[1], v1[1], 'observer2')
     
     def __observer1(self):
         v1, v2 = ((k, v) for k, v in self.files.items())
-        ioa, self.attemps, self.assest, self.comp1, self.comp2, self.dv1, self.dv2, self.sv1 = self.__compare(v1[1], v2[1])
+        self.__compare(v1[1], v2[1])
         self.oName = v1[0]
         if self.showAttemps:
             print('\n{}, attemps: {}'.format(v1[0], self.attemps))
@@ -41,13 +41,13 @@ class Dex(object):
     
     def __observer2(self):
         v1, v2 = ((k, v) for k, v in self.files.items())
-        ioa, self.attemps, self.assest, self.comp1, self.comp2, self.dv1, self.dv2 = self.__compare(v2[1], v1[1])
+        self.__compare(v2[1], v1[1])
         self.oName = v2[0]
         if self.showAttemps:
             print('\n{}, attemps: {}'.format(v2[0], self.attemps))
         self.__test(self.comp1, self.comp2)
     
-    def __compare(self, v1, v2):
+    def __compare(self, v1, v2, observer):
         if not v1 or not v2:
             return None
         cv1, cv2 = v1.copy(), v2.copy()
@@ -60,7 +60,7 @@ class Dex(object):
         for i, e1 in enumerate(cv1):
             xPlot.append(round(e1[0]))
             bars1.append(e1[1] - e1[0])
-            startV1.append([e1[0], e1[1] - e1[0], e1[2]])
+            startV1.append([e1[0], e1[1] - e1[0], e1[1], e1[2]])
             for e2 in cv2:
                 diffStart, diffEnd = abs(e1[0] - e2[0]), abs(e1[1] - e2[1])
                 if i == 2 and self.debug:
@@ -74,7 +74,11 @@ class Dex(object):
                     va1[i] += 1
                     cv2.remove(e2)
                     break
-        return agree/len(cv1)*100, [agree, len(cv1)], va1, xPlot, yPlot, bars1, bars2, startV1
+        self.attemps = [agree, len(cv1)]
+        self.assest = va1
+        self.comp1, self.comp2 = xPlot, yPlot
+        self.sv1 = startV1
+        self.data[observer] += startV1
     
     def __test(self, v1, v2):
         from sklearn.metrics import cohen_kappa_score
@@ -114,126 +118,29 @@ class Dex(object):
         plt.show()'''
         from bokeh.plotting import figure, output_file, show
         colormap = {'VF': 'red', 'N1': 'green', 'M1': 'blue', 'AM': 'purple', 'R': 'black'}
-        data = {'data':[],'observer':[],'colors':[]}
-        p = figure(title = 'Some')
-        for x in self.sv1:
-            data['data'].append(x[0])
-            data['observer'].append(0.25)
-            data['colors'].append(colormap[x[2]])
-            data['data'].append(x[0])
-            data['observer'].append(0.2)
-            data['colors'].append(colormap[x[2]])
-            p.line([x[0], x[0]+x[1]], [0.2, 0.2], line_color=colormap[x[2]], line_width=3)
-        colors = [colormap[x[2]] for x in self.sv1]
-        #data = [x[0] for x in self.sv1]
+        observermap = {'observer1': 0.25, 'observer2': 0.2}
+        markmap = {'observer1': 'hex', 'observer2': 'triangle'}
         
+        p = figure(title = 'Some')
         p.xaxis.axis_label = 'Timeline'
         p.yaxis.axis_label = 'Observer'
-        p.line(0, 1, legend_label="Rate", line_color="red", line_width=2)
-        p.line(0, 0, legend_label="Rate", line_color="red", line_width=2)
-        p.circle(data['data'], data['observer'],
-         color=data['colors'], fill_alpha=0.2, size=10)
-        #output_file("iris.html", title="iris.py example")
+        
+        data = {'data':[],'observer':[],'colors':[], 'marks': []}
+        for k, v in self.data.items():
+            for e in v:
+                data['data'].append(e[0])
+                data['observer'].append(observermap[k])
+                data['colors'].append(colormap[e[3]])
+                data['marks'].append(markmap[k])
+                p.line([e[0], e[0] + e[1]], [observermap[k], observermap[k] + 0.025], line_color=colormap[e[3]], line_width=3)
+                p.line([e[0], e[0] + e[1]], [observermap[k], observermap[k]], line_color=colormap[e[3]], line_width=3)
+            p.scatter(data['data'], data['observer'],
+         color=data['colors'], marker=data['marks'], legend_label=k, fill_alpha=0.2, size=10)
+            data = {'data':[],'observer':[],'colors':[], 'marks': []}
+        
+        p.line(0, 1, line_color="red", line_width=2)
+        p.line(0, 0, line_color="red", line_width=2)
         show(p)
-    
-    def graphGroupedBarplot(self):
-        import matplotlib.pyplot as plt
-        plt.figure(figsize=(10, 5), dpi=80)
-        barWidth = 0.25
-        r1 = np.arange(len(self.dv1))
-        r2 = [x + barWidth for x in r1]
-        plt.bar(r1, self.dv1, color='#7f6d5f', width=barWidth, edgecolor='white', label='var1')
-        plt.bar(r2, self.dv2, color='#557f2d', width=barWidth, edgecolor='white', label='var2')
-        plt.savefig('{0}/output/{1}_groupedbarplot.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def graphDensityPlot(self):
-        import matplotlib.pyplot as plt
-        sns.set(style="darkgrid")
-        fig = sns.kdeplot(self.dv1, shade=True, color='r')
-        fig = sns.kdeplot(self.dv2, shade=True, color='b')
-        plt.savefig('{0}/output/{1}_densityplot.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def graphDensityChart(self):
-        import matplotlib.pyplot as plt
-        plt.rcParams['figure.figsize'] = 12, 8
-        sns.set(style='whitegrid')
-        data = pd.DataFrame({
-            'a': self.dv1,
-            'b': self.dv2
-        })
-        data = pd.melt(data, var_name='observers', value_name='value')
-        sns.kdeplot(data=data, x='value', hue='observers', fill=True, common_norm=False, alpha=0.6, palette='viridis')
-        plt.savefig('{0}/output/{1}_densitychart.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def graphHeatmap(self):
-        import matplotlib.pyplot as plt
-        plt.figure(figsize=(45, 20), dpi=80)
-        df = pd.DataFrame({ 'a': self.dv1, 'b': self.dv2 })
-        sns.heatmap(df)
-        plt.savefig('{0}/output/{1}_heatmap.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def graphSpaghettiPlot(self):
-        import matplotlib.pyplot as plt
-        plt.figure(figsize=(45, 20), dpi=80)
-        df = pd.DataFrame({ 'x': [i for i, v in enumerate(self.dv1)], 'y1': self.dv1, 'y2': self.dv2 })
-        plt.style.use('seaborn-darkgrid')
-        palette = plt.get_cmap('Set1')
-        num = 1
-        for column in df.drop('x', axis=1):
-            num += 1
-            plt.plot(df['x'], df[column], marker='', color=palette(num), linewidth=1, alpha=0.9, label=column)
-        plt.legend(loc=2, ncol=1)
-        plt.title('Observer {0} - '.format(self.observer + 1), loc='left', fontsize=12, fontweight=0, color='orange')
-        plt.xlabel('Time')
-        plt.ylabel('Delta')
-        plt.savefig('{0}/output/{1}_spaghettiplot.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def graphLollipopPlot(self):
-        import matplotlib.pyplot as plt
-        plt.figure(figsize=(30, 20), dpi=80)
-        # Create a dataframe
-        df = pd.DataFrame({ 'group': [i for i, v in enumerate(self.dv1)], 'value1': self.dv1, 'value2': self.dv2 })
-        # Reorder it following the values of the first value:
-        ordered_df = df # df.sort_values(by='value1')
-        my_range = range(1, len(df.index) + 1)
-        # The horizontal plot is made using the hline function
-        plt.hlines(y=my_range, xmin=ordered_df['value1'], xmax=ordered_df['value2'], color='grey', alpha=0.4)
-        plt.scatter(ordered_df['value1'], my_range, color='skyblue', alpha=1, label='value1')
-        plt.scatter(ordered_df['value2'], my_range, color='green', alpha=0.4 , label='value2')
-        plt.legend()
-        # Add title and axis names
-        plt.yticks(my_range, ordered_df['group'])
-        plt.title('Comparison of the value 1 and the value 2', loc='left')
-        plt.xlabel('Value of the variables')
-        plt.ylabel('Group')
-        plt.savefig('{0}/output/{1}_lollipopplot.png'.format(self.src, self.observer + 1))
-        plt.cla()
-        plt.clf()
-        plt.close()
-    
-    def showAssest(self):
-        print('Assest: {}'.format(self.assest))
-    
-    def showComparation(self):
-        print('Observer 1: {}\nObserver 2: {}\n'.format(self.comp1, self.comp2))
-    
-    def showTable(self):
-        print(self.table)
     
     def kappa(self, printRes=True):
         kappa = self.ratingtask.kappa()
@@ -241,26 +148,12 @@ class Dex(object):
             print('Cohen\'s Kappa: {}%'.format(round(kappa * 100, 4)))
         return kappa
     
-    def kalpha(self, printRes=True):
-        alpha = self.ratingtask.alpha()
-        if printRes:
-            print('Krippendorff\'s alpha: {}%'.format(round(alpha * 100, 4)))
-        return alpha
-    
-    def spi(self, printRes=True):
-        pi = self.ratingtask.pi()
-        if printRes:
-            print('Scott\'s pi: {}%'.format(round(pi * 100, 4)))
-        return pi
-    
     def saveMetrics(self):
         filename = '{0}/output/metrics.txt'.format(self.src)
         with open(filename, 'a') as out:
             out.write('Observer {0}\n'.format(self.observer + 1))
             out.write('{0}, attemps: {1}\n'.format(self.oName, self.attemps))
             out.write('Cohen\'s Kappa: {0}%\n'.format(round(self.kappa(printRes=False) * 100, 4)))
-            out.write('Krippendorff\'s alpha: {0}%\n'.format(round(self.kalpha(printRes=False) * 100, 4)))
-            out.write('Scott\'s pi: {0}%\n\n'.format(round(self.spi(printRes=False) * 100, 4)))
 
 def getArgs():
     parser = ArgumentParser(prog='deadshot', description='Arguments being passed to the program')
@@ -272,27 +165,14 @@ def getArgs():
 
 def main():
     args = getArgs()
-    print(args)
     
     buffer, bLen = load(path=args.sample, rev=args.rev, limit=float(args.limit))
     if bLen == 2:
         offset = float(args.offset)
-        # Observer 1
         dex = Dex(buffer, observer=0, showAttemps=False, src=args.sample, offset=offset)
         dex.graphBrokenBarh()
-        #dex.saveMetrics()
-        
-        # Observer 2
-        dex = Dex(buffer, observer=1, showAttemps=False, src=args.sample, offset=offset)
-        #dex.saveMetrics()
     else:
         raise NotImplementedError('Only accepted two observers')
-    '''z = Measures(buffer, metric='label')
-    z.showTable()
-    z.mdpo()
-    z.kappa()
-    z.alpha()
-    z.pi()'''
 
 if __name__ == '__main__':
     main()
